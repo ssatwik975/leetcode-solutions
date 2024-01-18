4124d3862f65d7396651040c17d2deaebe56d762
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-01-18 19:17:38 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 8.3 MB (100.00%)

---
+++ solutions/0412-fizz-buzz/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> swag;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                swag.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                swag.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                swag.push_back("Buzz");
+            } else {
+                swag.push_back(to_string(i));
+            }
+        }
+        return swag;
+    }
+};
+
