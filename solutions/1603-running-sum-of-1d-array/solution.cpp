8064dcfa8d057a6dd94b65188c4b6486881341c8
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-01-18 18:37:09 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 8.8 MB (100.00%)

---
+++ solutions/1603-running-sum-of-1d-array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> swag(n);
+        swag[0] = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            swag[i] = swag[i-1] + nums[i];
+        }
+
+        return swag;
+        
+    }
+};
