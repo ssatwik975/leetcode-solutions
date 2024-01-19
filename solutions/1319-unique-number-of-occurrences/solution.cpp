40060e0ab3058edb7ddd72c3cab02963c8c34cb0
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-01-19 17:26:50 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 10.5 MB (100.00%)

---
+++ solutions/1319-unique-number-of-occurrences/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int maxval = *max_element(arr.begin(), arr.end());
+        int minval = *min_element(arr.begin(), arr.end());
+
+        vector<int> hash(maxval-minval+1, 0);
+
+        int n = arr.size();
+        for (int i = 0; i < n; i++){
+            hash[arr[i]-minval]++;
+        }
+        
+        sort(hash.begin(), hash.end());
+
+        int m = hash.size();
+        for(int i = 0; i < m-1; i++){
+            if(hash[i]!=0){
+                if (hash[i]==hash[i+1]){
+                return false;
+                }
+            }   
+            
+        }
+        return true;
+    }
+};
