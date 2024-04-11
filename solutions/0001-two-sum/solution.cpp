b77669d91f53bd2a04a8119b43064550b70c3fd8
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-11 12:59:18 +0000

    [Today's Leetcode Solutions] Runtime - 3 ms (76.75%), Memory - 14 MB (66.97%)

---
+++ solutions/0001-two-sum/solution.cpp
@@ -0,0 +1,24 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int,int> storedIndices;
+        for(int idx = 0; idx < nums.size(); idx++){
+            int complement = target - nums[idx];
+            if(storedIndices.count(complement)){
+                int mappedIndex = storedIndices[complement];
+                return {idx, mappedIndex};
+            }
+            storedIndices[nums[idx]] = idx;            
+        }
+        
+        return {};
+    }
+};
