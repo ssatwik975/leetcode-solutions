1a9c06196dcc754fa5d67a6cd53456450921cac4
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-11 00:34:30 +0000

    [Performance] Runtime - 11 ms (12.06%), Memory - 20.3 MB (99.96%)

--- solutions/0268-missing-number/solution.cpp
+++ solutions/0268-missing-number/solution.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int ans = nums.size();
-        for(int i = 0; i < nums.size(); i++){
-            ans = ans ^ i;
-            ans = ans ^ nums[i];
+        
+        int size = nums.size();
+        int total = size * (size+1) / 2;
+
+        int sum = 0;
+
+        for(int i=0; i < size; ++i ){
+            sum += nums[i];
         }
-        return ans;
 
-        // int size = nums.size();
-        // int total = size * (size+1) / 2;
-
-        // int sum = 0;
-
-        // for(int i=0; i < size; ++i ){
-        //     sum += nums[i];
-        // }
-
-        // return total - sum;
+        return total - sum;
         
     }
 };
