94174c5bc517b7e4ecfc2f6c9177a61c4585379e
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-11 21:47:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.7 MB (98.29%)

--- solutions/0031-next-permutation/solution.cpp
+++ solutions/0031-next-permutation/solution.cpp
@@ -1,45 +1,12 @@
-const auto init = []() {
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    std::ios_base::sync_with_stdio(false);
-    return nullptr;
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
 }();
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // next_permutation(nums.begin(), nums.end());
-        int n = nums.size(); // size of the array.
-
-        // Step 1: Find the break point:
-        int ind = -1; // break point
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                // index i is the break point
-                ind = i;
-                break;
-            }
-        }
-
-        // If break point does not exist:
-        if (ind == -1) {
-            // reverse the whole array:
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-
-        // Step 2: Find the next greater element
-        //         and swap it with arr[ind]:
-
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums[i], nums[ind]);
-                break;
-            }
-        }
-
-        // Step 3: reverse the right half:
-        reverse(nums.begin() + ind + 1, nums.end());
-
+        next_permutation(nums.begin(), nums.end());
     }
 };
-
