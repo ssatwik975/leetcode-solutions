4343ebd3fd377b4030d57a82e884f6a0b7f42ba2
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-11 00:34:30 +0000

    [Today's Leetcode Solutions] Runtime - 11 ms (12.06%), Memory - 20.3 MB (99.96%)

---
+++ solutions/0268-missing-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int size = nums.size();
+        int total = size * (size+1) / 2;
+
+        int sum = 0;
+
+        for(int i=0; i < size; ++i ){
+            sum += nums[i];
+        }
+
+        return total - sum;
+        
+    }
+};
