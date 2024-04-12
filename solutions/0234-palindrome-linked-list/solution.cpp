6dc8b7d1bab861b11f91f56c2257742eda3584c6
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-12 11:55:23 +0000

    Sync LeetCode submission Runtime - 173 ms (5.07%), Memory - 130.8 MB (19.66%)

--- solutions/0234-palindrome-linked-list/solution.cpp
+++ solutions/0234-palindrome-linked-list/solution.cpp
@@ -11,53 +11,14 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        // Find the middle of the linked list
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
-        // Reverse the second half of the linked list
-        ListNode* prev = nullptr;
-        ListNode* curr = slow;
-        ListNode* next = curr;
-        
-        while (curr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        
-        // Compare the first and second halves of the linked list
-        ListNode* first = head;
-        ListNode* second = prev;
-        
-        while (second != nullptr) {
-            if (first->val != second->val) {
-                return false;
-            }
-            first = first->next;
-            second = second->next;
-        }
-        
-        return true;
+    vector<int> v;
+    while(head) {
+        v.push_back(head->val);
+        head = head->next;
     }
+    for(int i = 0; i < v.size()/2; ++i) {
+        if(v[i] != v[v.size()-i-1]) return false;
+    }
+    return true;
+}
 };
-
-int init = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    ofstream out("user.out");
-    for (string s; getline(cin, s);)
-        out << (equal(s.begin() + 1, s.begin() + s.size() / 2, s.rbegin() + 1)
-                    ? "true\n"
-                    : "false\n");
-    out.flush();
-    exit(0);
-
-    return 0;
-}();
