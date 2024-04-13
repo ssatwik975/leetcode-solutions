05370ada30aa2d1bc33259105002bbe0e526b090
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-13 22:18:39 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 8 MB (99.35%)

---
+++ solutions/0118-pascals-triangle/solution.cpp
@@ -0,0 +1,27 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        vector<int> prevRow;
+        
+        for (int i = 0; i < numRows; i++) {
+            vector<int> currentRow(i + 1, 1);
+            
+            for (int j = 1; j < i; j++) {
+                currentRow[j] = prevRow[j - 1] + prevRow[j];
+            }
+            
+            result.push_back(currentRow);
+            prevRow = currentRow;
+        }
+        
+        return result;
+    }
+};
+
