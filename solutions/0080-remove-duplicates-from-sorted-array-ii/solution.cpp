388b4cfb3db067dc112e3453aaa7a93fc3f5b71f
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-14 16:13:26 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 13.6 MB (87.25%)

---
+++ solutions/0080-remove-duplicates-from-sorted-array-ii/solution.cpp
@@ -0,0 +1,17 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    return i;
+    }
+};
