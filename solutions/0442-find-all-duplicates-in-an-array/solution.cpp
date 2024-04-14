be55b436b950f51e5a91c9215a31ababbbf14da0
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-14 15:48:12 +0000

    [Today's Leetcode Solutions] Runtime - 28 ms (31.26%), Memory - 35.7 MB (99.96%)

---
+++ solutions/0442-find-all-duplicates-in-an-array/solution.cpp
@@ -0,0 +1,19 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        if(nums.empty())return {};
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])-1]<0)
+                ans.push_back(abs(nums[i]));
+            nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+        }
+        return ans;
+    }
+};
