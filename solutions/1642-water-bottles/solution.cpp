a702898542eb71de485697a61ff7d32310fea7c5
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-15 19:13:22 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 7.2 MB (98.03%)

---
+++ solutions/1642-water-bottles/solution.cpp
@@ -0,0 +1,24 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drunk = numBottles;
+        int empty = numBottles;
+        int flag = 1;
+        while(flag){
+            flag = 0;
+            if (numExchange <= empty) {
+                empty -= numExchange;
+                drunk++;
+                empty++;
+                flag = 1;
+            }
+        }
+        return drunk;
+    }
+};
