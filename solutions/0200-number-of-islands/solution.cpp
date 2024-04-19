28c00855c4b239a7ae5b0b266b0038c380ea5c51
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-19 21:23:03 +0000

    Sync LeetCode submission Runtime - 19 ms (96.64%), Memory - 15.7 MB (100.00%)

--- solutions/0200-number-of-islands/solution.cpp
+++ solutions/0200-number-of-islands/solution.cpp
@@ -1,42 +1,34 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& visited) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        // Out of bounds or water or already visited cell
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '0' || visited[i][j] == 1) 
-            return;
-
-        // Mark this cell as visited
-        visited[i][j] = 1;
-
-        // Visit all 4 possible directions
-        dfs(i + 1, j, grid, visited);
-        dfs(i - 1, j, grid, visited);
-        dfs(i, j + 1, grid, visited);
-        dfs(i, j - 1, grid, visited);
+    
+    void islandgayab(vector<vector<char>>& grid, int i, int j, int m, int n){
+        if (i<0 || j<0 || i==m || j==n || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        islandgayab(grid,i+1,j,m,n);
+        islandgayab(grid,i,j+1,m,n);
+        islandgayab(grid,i-1,j,m,n);
+        islandgayab(grid,i,j-1,m,n);
+        return;
     }
-
+    
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        if (n == 0) return 0;
-        int m = grid[0].size();
-        
-        int count = 0;
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                // Start a DFS if we find an unvisited land cell
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(i, j, grid, visited);
-                    count++;
+        int m = grid.size();
+        int n = grid[0].size();
+        int res=0;
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (grid[i][j]=='1'){
+                    res++;
+                    islandgayab(grid,i,j,m,n);
                 }
             }
         }
-
-        return count;
+        return res;
     }
 };
-
