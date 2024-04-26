c8dc18cf0b6a6fb85da03d91a0d06218f8fce74b
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-26 20:11:18 +0000

    [Today's Leetcode Solutions] Runtime - 41 ms (51.42%), Memory - 11.6 MB (99.19%)

---
+++ solutions/2444-longest-ideal-subsequence/solution.cpp
@@ -0,0 +1,19 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        int dp[150] = {}, res = 0;
+        for (auto& i : s) {
+            for (int j = i - k; j <= i + k; ++j)
+                dp[i] = max(dp[i], dp[j]);
+            res = max(res, ++dp[i]);
+        }
+        return res;
+    }
+};
