c78c7c205a28fdf589d1eb0b8ca2ad1207dafde6
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-04-26 20:10:33 +0000

    [Performance] Runtime - 0 ms (100.00%), Memory - 7.3 MB (97.56%)

--- solutions/1236-n-th-tribonacci-number/solution.cpp
+++ solutions/1236-n-th-tribonacci-number/solution.cpp
@@ -1,3 +1,10 @@
+const auto init = [](){
+       std::cin.tie(nullptr);
+       std::cout.tie(nullptr);
+       std::ios_base::sync_with_stdio(false);
+       return nullptr;
+}();
+
 class Solution {
 public:
     int tribonacci(int n) {
