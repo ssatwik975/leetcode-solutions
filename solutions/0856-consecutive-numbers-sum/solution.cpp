bbd4f896f62ee01b7bdc4765398123f4c34fc66d
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-05-04 09:10:15 +0000

    [Performance] Runtime - 0 ms (100.00%), Memory - 7.2 MB (100.00%)

--- solutions/0856-consecutive-numbers-sum/solution.cpp
+++ solutions/0856-consecutive-numbers-sum/solution.cpp
@@ -1,8 +1,16 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
 class Solution {
-public:
-    int consecutiveNumbersSum(int n) {
-        int c=1;
-        for(int i=2;i<sqrt(2*n);++i) {if ((n-(i*(i-1)/2)) % i == 0) c++;}
-        return c;
-    }
+ public:
+  int consecutiveNumbersSum(int n) {
+    int ans = 0;
+    for (int i = 1, triangleNum = i; triangleNum <= n; ++i, triangleNum += i)
+      if ((n - triangleNum) % i == 0)
+        ++ans;
+    return ans;
+  }
 };
