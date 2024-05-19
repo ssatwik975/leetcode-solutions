23604e0fa02cfa486383bc8e97a88381ebe4ecaf
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-05-19 00:00:41 +0000

    Sync LeetCode submission Runtime - 20 ms (9.20%), Memory - 22.7 MB (98.06%)

--- solutions/0056-merge-intervals/solution.cpp
+++ solutions/0056-merge-intervals/solution.cpp
@@ -1,5 +1,3 @@
-#pragma GCC optimize("Ofast, unroll-loops, no-stack-protector, fast-math, O3, tree-vectorize, inline, rename-registers")
-auto speedup = []{ios::sync_with_stdio(0); cin.tie(0); std::cout.tie(nullptr); return(0);}();
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -8,17 +6,21 @@ public:
         sort(intervals.begin(),intervals.end());
 
         vector<vector<int>>ans;
-        ans.emplace_back(intervals[0]);
+        ans.push_back(intervals[0]);
 
         for(int i=1;i<n;i++){
 
             if(ans.back()[1]>=intervals[i][0]){
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+               
             }
             else{
-                ans.emplace_back(intervals[i]);
+                ans.push_back(intervals[i]);
             }
+
         }
+
         return ans;
+        
     }
 };
