4a9523902d02f432928b095b4cf74e660414fc35
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-05-31 20:08:08 +0000

    [Performance] Runtime - 92 ms (13.00%), Memory - 69.9 MB (100.00%)

--- solutions/1370-count-number-of-nice-subarrays/solution.cpp
+++ solutions/1370-count-number-of-nice-subarrays/solution.cpp
@@ -1,10 +1,6 @@
-#pragma GCC optimize("O3","unroll-loops")
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
         int start = 0; int end = 0; int ans = 0;
         int counter = 0;
         while(end<nums.size()){
