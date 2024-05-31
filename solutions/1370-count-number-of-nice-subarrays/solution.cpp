9151a5f2ecda642aa32c6419e783c1ac1a0942b0
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-05-31 20:08:08 +0000

    [Today's Leetcode Solutions] Runtime - 92 ms (13.00%), Memory - 69.9 MB (100.00%)

---
+++ solutions/1370-count-number-of-nice-subarrays/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int start = 0; int end = 0; int ans = 0;
+        int counter = 0;
+        while(end<nums.size()){
+            if(nums[end]%2){
+                k--;
+                counter = 0;
+            }
+            while(k == 0){
+                counter++;
+                k = k + (nums[start]%2);
+                start++;
+            }
+            ans += counter;
+            end++;
+        }
+        return ans;
+    }
+};
