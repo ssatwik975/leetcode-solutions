a28c29973e6d706932c533c24d6dce6a85affeef
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-01 00:05:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (99.90%)

--- solutions/3379-score-of-a-string/solution.cpp
+++ solutions/3379-score-of-a-string/solution.cpp
@@ -3,8 +3,8 @@ public:
     int scoreOfString(string s) 
     {
         int res = 0, n = s.size();
-        for(int i=0;i<n-1;i++)
-            res += abs((s[i]-'a') - (s[i+1]-'a'));     
+        for(int i=1;i<n;i++)
+            res += abs((s[i] - s[i-1]));     
         return res;
     }
 };
