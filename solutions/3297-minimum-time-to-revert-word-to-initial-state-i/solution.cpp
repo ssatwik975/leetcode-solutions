aef43c1d3f8653651dc8001c08d915eb7ac49c54
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-04 16:41:07 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 7.9 MB (100.00%)

---
+++ solutions/3297-minimum-time-to-revert-word-to-initial-state-i/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTimeToInitialState(string word, int k) {
+        int n = word.length();
+        int count = 1;
+        for(int i = k; i < n; i+=k){
+            if (word.compare(i, n-i, word, 0, n-i) == 0)
+                break;
+            count++;
+        }
+        return count;
+    }
+};
