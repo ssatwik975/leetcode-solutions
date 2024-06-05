84aea2235ba73835dc5f82ab13b1437e167ac8b9
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-05 14:20:29 +0000

    Sync LeetCode submission Runtime - 20 ms (9.20%), Memory - 22.9 MB (90.73%)

--- solutions/0056-merge-intervals/solution.cpp
+++ solutions/0056-merge-intervals/solution.cpp
@@ -6,21 +6,17 @@ public:
         sort(intervals.begin(),intervals.end());
 
         vector<vector<int>>ans;
-        ans.push_back(intervals[0]);
+        ans.emplace_back(intervals[0]);
 
         for(int i=1;i<n;i++){
 
             if(ans.back()[1]>=intervals[i][0]){
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-               
             }
             else{
-                ans.push_back(intervals[i]);
+                ans.emplace_back(intervals[i]);
             }
-
         }
-
         return ans;
-        
     }
 };
