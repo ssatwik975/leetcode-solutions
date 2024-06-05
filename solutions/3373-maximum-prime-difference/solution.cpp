4df3bcb17262b51f1d360b89f22fd795edb58280
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-05 09:54:42 +0000

    Sync LeetCode submission Runtime - 49 ms (99.17%), Memory - 107.9 MB (87.26%)

--- solutions/3373-maximum-prime-difference/solution.cpp
+++ solutions/3373-maximum-prime-difference/solution.cpp
@@ -1,55 +1,45 @@
-const auto init = [](){
-       std::cin.tie(nullptr);
-       std::cout.tie(nullptr);
-       std::ios_base::sync_with_stdio(false);
-       return nullptr;
+const auto init = []() {
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    return nullptr;
 }();
+
+const vector<bool> isPrime(int n) {
+    vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                prime[i] = false;
+            }
+        }
+    }
+    return prime;
+}
+const vector<bool> prime = isPrime(100);
+
 class Solution {
 public:
-    
-    vector<bool> isPrime(int n){
-        vector<bool> prime(n+1, true);
-        prime[0] = prime[1] = false;
-        for(int i=2; i*i<=n; i++){
-            if(prime[i]){
-                for(int j=i*i; j<=n; j+=i){
-                    prime[j] = false;
-                }
-            }
-        }
-        return prime;
-    }
-
     int maximumPrimeDifference(vector<int>& nums) {
-       int n = nums.size();
-        vector<bool> prime = isPrime(100);
+        int n = nums.size();
         int first = -1, last = -1;
 
-        for(int i=0; i<n; i++)
-        {
-            if(prime[nums[i]]){
-                if(first == -1){
-                    first = i;
-                    break;
-                }
+        for (int i = 0; i < n; i++) {
+            if (prime[nums[i]]) {
+                first = i;
+                break;
             }
         }
 
-        for(int i=n-1; i>=0; i--){
-            if(prime[nums[i]]){
-                if(last == -1){
-                    last = i;
-                    break;
-                }
+        for (int i = n - 1; i >= 0; i--) {
+            if (prime[nums[i]]) {
+                last = i;
+                break;
             }
-        }
-
-        if(first == -1){
-            return 0;
         }
 
         return last - first;
-
     }
 };
 
