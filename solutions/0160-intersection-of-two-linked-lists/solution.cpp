5c8dca39f839c5ba3d2889be684fcf1064b37d25
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-05 09:09:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.5 MB (100.00%)

--- solutions/0160-intersection-of-two-linked-lists/solution.cpp
+++ solutions/0160-intersection-of-two-linked-lists/solution.cpp
@@ -16,22 +16,24 @@ int init = []
 class Solution {
 public:
 	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-		ListNode *ptr1 = headA;
-		ListNode *ptr2 = headB;
-		while(ptr1 != ptr2){
-			if(ptr1 == NULL){
-				ptr1 = headB;
-			}
-			else{
-				ptr1 = ptr1 -> next;
-			}
-			if(ptr2 == NULL){
-				ptr2 = headA;
-			}
-			else{
-				ptr2 = ptr2 -> next;
-			}
-		}
-		return ptr1;
+		ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        if (headA == NULL || headB == NULL) {
+            return NULL;
+        }
+        unordered_map<ListNode*, int> mapp;
+        while(headA != NULL){
+            mapp[headA]++;
+            headA = headA -> next;
+        }
+        while(headB != NULL){
+            if(mapp.count(headB)){
+                return headB;
+            }else{
+                headB = headB -> next;
+            }
+        }
+		return 0;
 	}
 };
