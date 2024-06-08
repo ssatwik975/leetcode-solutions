0c9503bafa911235241ad0efcd20c6adf12cc2d2
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-08 12:34:08 +0000

    Sync LeetCode submission Runtime - 37 ms (25.90%), Memory - 63.3 MB (100.00%)

--- solutions/0287-find-the-duplicate-number/solution.cpp
+++ solutions/0287-find-the-duplicate-number/solution.cpp
@@ -1,23 +1,51 @@
+static const bool Init = []() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+
+inline bool is_digit(char c) { return (c >= '0' && c <= '9'); }
+
+std::vector<int> parse_input_data_string(const std::string& s) {
+    std::vector<int> input_data_1D;
+    const int N = s.size();
+    int left = 0;
+    while (left < N) {
+        if (!is_digit(s[left])) {
+            ++left;
+            continue;
+        }
+        int right = left;
+        int value = 0;
+        while (right < N && is_digit(s[right])) {
+            value = value * 10 + (s[right] - '0');
+            ++right;
+        }
+        input_data_1D.push_back(value);
+        left = right;
+    }
+    return input_data_1D;
+}
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        int n = nums.size();
-        vector<int> swag (n,0);
-        int ans;
-        for(int i = 0; i<n; i++){
-            swag[i] = nums[i];
-        }
-
-        for(auto it: swag){
-            if (swag[abs(it) - 1] < 0){
-                ans = abs(it);
-            } else {
-                swag[abs(it)-1] *= -1;
+        if (nums.size() > 1) {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
             }
 
+            fast = 0;
+            while (fast != slow) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
         }
-        return ans;
+        return -1;
     }
 };
