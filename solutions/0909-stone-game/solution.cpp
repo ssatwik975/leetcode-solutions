eb1107e31e37839e550bc304ff527166f0ad1ec9
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-11 11:28:24 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 9.6 MB (93.90%)

---
+++ solutions/0909-stone-game/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+                ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        return true;
+    }
+};
