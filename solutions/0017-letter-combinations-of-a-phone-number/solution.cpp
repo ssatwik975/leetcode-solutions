a9586927682512128d38a0cdfe23ffba6e90ebe4
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-16 07:45:36 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 8.1 MB (98.59%)

---
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -0,0 +1,24 @@
+int speedup = []{ios::sync_with_stdio(0); cin.tie(0); return 0;}();
+class Solution {
+public:
+    const vector<string> pad = {"",    "",    "abc",  "def", "ghi",
+                                "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+        vector<string> result;
+        result.push_back("");
+
+        for (auto digit : digits) {
+            vector<string> tmp;
+            for (auto candidate : pad[digit - '0']) {
+                for (auto s : result) {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        return result;
+    }
+};
