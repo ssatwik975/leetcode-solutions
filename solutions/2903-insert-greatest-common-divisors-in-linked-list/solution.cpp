b0d099cd4cfb5f1ace67c4e57837adfa0553bdde
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-06-25 16:52:22 +0000

    [Today's Leetcode Solutions] Runtime - 7 ms (41.83%), Memory - 8 MB (100.00%)

---
+++ solutions/2903-insert-greatest-common-divisors-in-linked-list/solution.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        ListNode* first = head;
+        ListNode* second = head->next;
+        while(second){
+            ListNode* gcda = new ListNode(gcd(first->val, second->val));
+            first->next = gcda;
+            gcda->next = second;
+            first = first->next->next;
+            second = second -> next;
+        }
+        return head;
+    }
+};
+
+
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+int init = [] {
+    ofstream out("user.out");
+    cout.rdbuf(out.rdbuf());
+    for (string str; getline(cin, str); cout << '\n') {
+        if (str.find(',') == string::npos) {
+            cout << str;
+            continue;
+        }
+        stringstream ss(str); ss.ignore();
+        int prev; ss >> prev; ss.ignore();
+        cout << '[' << prev;
+        for (int curr; ss >> curr; ss.ignore()) {
+            cout << ',' << gcd(prev, curr) << ',' << curr;
+            prev = curr;
+        }
+        cout << ']';
+    }
+    exit(0);
+    return 0;
+}();
