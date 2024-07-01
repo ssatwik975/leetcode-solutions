cc9237e9b4cb398c2e0b8ebf3955a9c45431d098
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-01 08:07:51 +0000

    [Performance] Runtime - 75 ms (27.25%), Memory - 52.7 MB (99.97%)

--- solutions/0131-palindrome-partitioning/solution.cpp
+++ solutions/0131-palindrome-partitioning/solution.cpp
@@ -1,27 +1,5 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> pars;
-        vector<string> par;
-        partition(s, 0, par, pars);
-        return pars;
-    }
-private: 
-    void partition(string& s, int start, vector<string>& par, vector<vector<string>>& pars) {
-        int n = s.length();
-        if(start==n){
-            pars.push_back(par);
-        }else{
-            for(int i = start ; i < n ;i ++){
-               if(isPalindrome(s,start,i)){
-                   par.push_back(s.substr(start,i-start+1));
-                   partition(s,i+1,par,pars);
-                   par.pop_back();
-               }
-            }
-        }
-    }
-    
     bool isPalindrome(string& s, int l, int r) {
         while (l < r) {
             if (s[l++] != s[r--]) {
@@ -29,5 +7,25 @@ private:
             }
         }
         return true;
+    }
+    void solve(string& s, int part, vector<string>& ds, vector<vector<string>>& ans) {
+        int n = s.length();
+        if (part == n) {
+            ans.push_back(ds);
+            return;
+        }
+        for (int i = part; i < n; i++) {
+            if (isPalindrome(s, part, i)) {
+                ds.push_back(s.substr(part, i - part + 1));
+                solve(s, i + 1, ds, ans);
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> ds;
+        solve(s, 0, ds, ans);
+        return ans;
     }
 };
