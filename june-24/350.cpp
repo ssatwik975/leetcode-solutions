6ccdbcabb2c53cf7b6252b6236f4b0c2fecc5948
s000ik <s000ik@users.noreply.github.com>
2024-07-02 21:34:29 +0530

    June 2nd

---
+++ june-24/350.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_map<int,int>mpp;
+        for(auto x: nums1){
+            mpp[x]++;
+        }
+        for(auto x : nums2){
+            if(mpp.find(x) != mpp.end()){
+                mpp[x]--;
+                if(mpp[x]>=0){
+                    ans.push_back(x);
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
