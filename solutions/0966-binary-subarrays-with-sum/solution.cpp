0a1da9123415a6db88a4391780f0d9124ba66973
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-03 00:26:55 +0000

    [Today's Leetcode Solutions] Runtime - 16 ms (38.74%), Memory - 31.1 MB (99.93%)

---
+++ solutions/0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int func(vector<int>&nums, int goal){
+        if(goal < 0){
+            return 0;
+        }
+
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int count = 0;
+
+        while(right < nums.size()){
+            sum += nums[right];
+            while(sum > goal){
+                sum -= nums[left++];
+            }
+            count += (right-left) + 1;
+            right++;
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums, goal) - func(nums, goal-1);
+    }
+};
