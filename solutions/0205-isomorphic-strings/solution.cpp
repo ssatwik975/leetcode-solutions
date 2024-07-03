5702cf13b25203ceda1cd5b0d4c4c1c986b5e456
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-03 11:38:51 +0000

    [Today's Leetcode Solutions] Runtime - 4 ms (28.59%), Memory - 9.3 MB (60.70%)

---
+++ solutions/0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+         unordered_map<char, char> mp, mp2;
+        for (int i=0; i<s.length(); ++i) {
+
+            if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
+            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
+            
+            mp[s[i]]=t[i];
+            mp2[t[i]]=s[i];
+        }
+        return true;
+    }
+};
+
+
+
+// WOW, you can map char to char like a dictionary. WOWOWOW
