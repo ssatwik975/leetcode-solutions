0a6a5d55c04dd7dc7e91fcf2c20ef93e1e5f2c23
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-04 16:25:50 +0000

    [Performance] Runtime - 7 ms (12.04%), Memory - 19.5 MB (100.00%)

--- solutions/0135-candy/solution.cpp
+++ solutions/0135-candy/solution.cpp
@@ -1,28 +1,63 @@
-int speedup = []{ios::sync_with_stdio(0); cin.tie(0); return 0;}();
+int speedup = [] {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+//         vector<int> ans(n, 1);
+
+//         for (int i = 1; i < n; i++) {
+//             if (ratings[i] > ratings[i - 1]) {
+//                 ans[i] = ans[i - 1] + 1;
+//             }
+//         }
+
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (ratings[i] > ratings[i + 1]) {
+//                 ans[i] = max(ans[i], ans[i + 1] + 1);
+//             }
+//         }
+
+//         int sum = 0;
+//         for (int i : ans) {
+//             sum += i;
+//         }
+//         return sum;
+//     }
+// };
 
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+        int sum = 1;
         int n = ratings.size();
-        vector<int> ans(n, 1);
-        
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                ans[i] = ans[i - 1] + 1;
+        int i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                sum += 1;
+                i++;
+                continue;
             }
-        }
-        
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                ans[i] = max(ans[i], ans[i + 1] + 1);
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                sum += peak;
+                i++;
             }
-        }
-
-        int sum = 0;
-        for (int i : ans) {
-            sum += i;
+            int down = 1;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                sum += down;
+                down++;
+                i++;
+            }
+            if (down > peak) {
+                sum += down - peak;
+            }
         }
         return sum;
     }
 };
-
