8ecd43faec2244a91a7c603e9760b088af4011fb
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-05 11:24:23 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

--- solutions/0232-implement-queue-using-stacks/solution.cpp
+++ solutions/0232-implement-queue-using-stacks/solution.cpp
@@ -1,38 +1,39 @@
-#pragma GCC optimize("O3", "unroll-loops")
 class MyQueue {
-    stack<int> s0, s1;
-    int front = 0;
-
 public:
-    MyQueue() {}
-
-    void push(int x) {
-        if (s0.empty())
-            front = x;
-        s0.push(x);
+    stack<int> input, output;
+    MyQueue() {
+        
     }
-
-    int pop() { // amortized O(1)
-        if (s1.empty()) {
-            while (!s0.empty()) {
-                int x = s0.top();
-                s0.pop();
-                s1.push(x);
+    
+    void push(int x) {
+       input.push(x); 
+    }
+    
+    int pop() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
             }
         }
-        int x = s1.top();
-        s1.pop();
-        return x;
+        int ans = output.top();
+        output.pop();
+        return ans;
     }
-
+    
     int peek() {
-        if (!s1.empty()) {
-            return s1.top();
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
         }
-        return front;
+        return output.top();
     }
-
-    bool empty() { return s0.empty() && s1.empty(); }
+    
+    bool empty() {
+        return (input.empty() && output.empty());
+    }
 };
 
 /**
