013cb2f3edd2a6132f1ce084f926eb4bbb1eb5f8
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-08 13:59:42 +0000

    [Performance] Runtime - 15 ms (5.03%), Memory - 26.2 MB (99.80%)

--- solutions/0124-binary-tree-maximum-path-sum/solution.cpp
+++ solutions/0124-binary-tree-maximum-path-sum/solution.cpp
@@ -1,21 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int tillm(TreeNode* root, int& ans) {
-        if (!root)
-            return 0;
-        int a = tillm(root->left, ans);
-        int b = tillm(root->right, ans);
-        if (a < 0)
-            a = 0;
-        if (b < 0)
-            b = 0;
-        if (a + b + root->val > ans)
-            ans = a + b + root->val;
-        return root->val += max(a,b);
-    }
     int maxPathSum(TreeNode* root) {
-        int ans = -2147483648;
-        tillm(root, ans);
-        return ans;
+        int maxi = INT_MIN;
+        maxPath(root, maxi);
+        return maxi;
+    }
+
+    int maxPath(TreeNode* node, int &maxi){
+        if(!node) return 0;
+
+        int left = maxPath(node->left, maxi);
+        int right = maxPath(node->right, maxi);
+        if(left < 0) left = 0;
+        if(right < 0) right = 0;
+        maxi = max(maxi, left + right + node->val);
+        return max(left, right) + node->val;
     }
 };
