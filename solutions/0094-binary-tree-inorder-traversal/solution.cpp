b636495568ae8899fefabcd221189dcd643a356f
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-13 17:21:23 +0000

    [Performance] Runtime - 0 ms (100.00%), Memory - 10.1 MB (91.38%)

--- solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -6,22 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& arr){
-        if(root == NULL){
-            return;
-        }
-        inorder(root->left, arr);
-        arr.push_back(root->val);
-        inorder(root->right, arr);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>arr;
-        inorder(root, arr);
-        return arr;
+        vector<int> inorder;
+        TreeNode* cur = root;
+
+        while (cur != NULL) {
+            if (cur->left == NULL) {
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                TreeNode* prev = cur->left;
+
+                while (prev->right && prev->right != cur) {
+                    prev = prev->right;
+                }
+
+                if (prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                } else {
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return inorder;
     }
 };
