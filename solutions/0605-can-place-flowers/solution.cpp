9d5c80f26b4208719bbb09dbe2ecf4ee3ae3f22a
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-07-16 16:27:15 +0000

    [Today's Leetcode Solutions] Runtime - 12 ms (4.54%), Memory - 23.2 MB (67.56%)

--- solutions/0605-can-place-flowers/solution.cpp
+++ solutions/0605-can-place-flowers/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
-        
-        for(int i = 0; i < flowerbed.size(); i++){
-            if(flowerbed[i] == 0){
-                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
-                int next = (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
-                
-                if(prev == 0 && next == 0){
-                    flowerbed[i] = 1;
-                    count++;
-                }
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        for(int i = 1; i < flowerbed.size()-1; ++i)
+        {
+            if(flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0)
+            {
+                --n;
+                ++i;
             }
+                
         }
-        return count >= n;
+        return n <=0;
     }
 };
