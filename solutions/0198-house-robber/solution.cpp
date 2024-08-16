c52ef885ffb222264aba52d892b82f3a0056febc
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-08-16 14:21:13 +0000

    [Today's Leetcode Solutions] Runtime - 0 ms (100.00%), Memory - 9.4 MB (87.55%)

--- solutions/0198-house-robber/solution.cpp
+++ solutions/0198-house-robber/solution.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int sum = 0;
         int n = nums.size();
-        vector <int> dp (n);
-        
-        for(int i = 0; i < n; i++){
-            dp[i] = nums[i];
+        if(n == 1) return nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
+        int curr = 0;
+        for(int i = 1; i < n; i++){
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int nontake = prev;
+            curr = max(take, nontake);
+            prev2 = prev;
+            prev = curr;
         }
-
-        for(int i = 2; i<n; i++){
-            int twojump = 0;
-            int threejump = 0;
-             for (int j = 0; j <= i - 2; j++) {
-                twojump = max(twojump, dp[j]);
-            }
-
-            for (int k = 0; k <= i - 3; k++) {
-                threejump = max(threejump, dp[k]);
-            }
-
-
-            dp[i] += max(twojump, threejump);
-        }
-
-
-
-        int ans = *max_element(dp.begin(), dp.end());
-        return ans;
+        return curr;
     }
 };
