39a11199c990fcdd422344c9d1224cdc0ae08450
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2024-08-20 08:36:55 +0000

    [Performance] Runtime - 0 ms (100.00%), Memory - 7.5 MB (83.21%)

--- solutions/1236-n-th-tribonacci-number/solution.cpp
+++ solutions/1236-n-th-tribonacci-number/solution.cpp
@@ -11,16 +11,19 @@ public:
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
 
-        vector<int> dp(n + 1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-
+        
+        int one = 0;
+        int two = 1;
+        int three = 1;
+        int curr = 0;
         for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            curr = one + two + three;
+            one = two;
+            two = three;
+            three = curr;
         }
 
-        return dp[n];
+        return curr;
     }
 };
 
