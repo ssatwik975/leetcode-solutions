4241c76ed939505c19554bbbd90c274f84230f7d
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2025-01-04 01:38:49 +0000

    [Today's Leetcode Solutions] Runtime - 715 ms (23.63%), Memory - 13.8 MB (96.63%)

---
+++ solutions/2059-unique-length-3-palindromic-subsequences/solution.cpp
@@ -0,0 +1,26 @@
+bool sub(const string& pattern, const string& text) {
+    int i, j;
+    for (i = 0, j = 0; i < text.size() && j < 3; ++i) {
+        if (text[i] == pattern[j])
+            ++j;
+    }
+    return j == 3;
+};
+
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int ans = 0;
+        for (char a = 'a'; a <= 'z'; ++a) {
+            for (char b = 'a'; b <= 'z'; ++b) {
+                string str;
+                str += a;
+                str += b;
+                str += a;
+                ans += sub(str, s);
+            }
+        }
+        return ans;
+    }
+};
+
