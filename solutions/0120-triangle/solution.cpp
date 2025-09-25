9486bb0ee71cce7addb69087f02f4e9ce92d2ff3
Satwik Singh <93423572+s000ik@users.noreply.github.com>
2025-09-25 08:38:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (80.63%)

--- solutions/0120-triangle/solution.cpp
+++ solutions/0120-triangle/solution.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        int ans = INT_MAX;
-        vector<int> prev(m, INT_MAX);
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = triangle[i].size() - 1; j >= 0; j--) {
-                if (i == 0) {
-                    prev[j] = triangle[i][j];
-                } else {
-                    int first = INT_MAX;
-                    if (j > 0) first = prev[j-1];
-                    int second = prev[j];
-
-                    prev[j] = triangle[i][j] + min(first, second);
-                }
-                if (i == m - 1) {
-                    ans = min(ans, prev[j]);
-                }
+        int n = triangle.size();
+        vector<int> minlen(triangle.back());
+        for (int layer = n - 2; layer >= 0; layer--) // For each layer
+        {
+            for (int i = 0; i <= layer; i++) // Check its every 'node'
+            {
+                // Find the lesser of its two children, and sum the current
+                // value in the triangle with it.
+                minlen[i] = min(minlen[i], minlen[i + 1]) + triangle[layer][i];
             }
         }
-        return ans;
+        return minlen[0];
     }
 };
-
